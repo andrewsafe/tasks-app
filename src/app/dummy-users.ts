b0d7cc6128346ccc4af192b8503785cf4f2f0c18
Andrew Safe <andrewsafe@gmail.com>
2024-10-27 16:07:58 -0400

    commit 01

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Andrew S',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Adam J',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Yousef N',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Joe M',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Sikander R',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'James A',
+    avatar: 'user-6.jpg',
+  },
+];
